c375c10b6aa6a859907e5fa8ac2c8a35463374e0
SulanAdam <sulan.adam19@gmail.com>
2023-04-27 13:10:52 +0200

    Bemeneti értékek vizsgálva, gitignore és gitkeep hozzáadva.

--- src/Idomunka.java
+++ src/Idomunka.java
@@ -9,10 +9,22 @@ public class Idomunka {
     }
     private double bekerData(){
         String megmunkaltHosszStr=beker("Kérem adja meg a megmunkált hosszt (mm-ben): ");
+        while (!checkInput(megmunkaltHosszStr)){
+            System.out.println("Hiba! A megadott adat formátumának double-nak kell lennie.");
+            megmunkaltHosszStr=beker("Kérem adja meg a megmunkált hosszt (mm-ben): ");}
         double megmunkaltHossz=Double.parseDouble(megmunkaltHosszStr);
+
+
         String fordulatonkentiElotolasStr=beker("Kérem adja meg a fordulatonkénti előtolást (mm-ben): ");
+        while (!checkInput(fordulatonkentiElotolasStr)){
+            System.out.println("Hiba! A megadott adat formátumának double-nak kell lennie.");
+            fordulatonkentiElotolasStr=beker("Kérem adja meg a fordulatonkénti előtolást (mm-ben): ");}
         double fordulatonkentiElotolas=Double.parseDouble(fordulatonkentiElotolasStr);
+
         String foorsoFordulatszamStr=beker("Kérem adja meg a főorsó fordulatszámát (fordulatszám/perc-ben): ");
+        while (!checkInput(foorsoFordulatszamStr)){
+            System.out.println("Hiba! A megadott adat formátumának double-nak kell lennie.");
+            foorsoFordulatszamStr=beker("Kérem adja meg a főorsó fordulatszámát (fordulatszám/perc-ben): ");}
         double foorsoFordulatszam=Double.parseDouble(foorsoFordulatszamStr);
         return szamol(megmunkaltHossz, fordulatonkentiElotolas, foorsoFordulatszam);
     }
@@ -24,5 +36,12 @@ public class Idomunka {
     private double szamol(double megMunkalthossz, double fordulatonkentiElotolas, double foorsoFordulatszam){
         double megmunkalasiIdo=megMunkalthossz/(fordulatonkentiElotolas*foorsoFordulatszam);
         return megmunkalasiIdo;
+    }
+    public boolean checkInput(String input){
+        if(input.matches("[0-9.]*")){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
