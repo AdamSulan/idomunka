71a164fd5f178fe58958e2b85731ad692ff79cb6
SulanAdam <sulan.adam19@gmail.com>
2023-04-27 13:01:55 +0200

    Bekérés, számolás és kiírás létrehozva

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Idomunka idomunka=new Idomunka();
+        idomunka.startIdomunka();
     }
 }
---
+++ src/Idomunka.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Idomunka {
+    public void startIdomunka(){
+        kiir(bekerData());
+    }
+    private void kiir(double megmunkalasiIdo){
+        System.out.println("A megmunkálási idő: "+megmunkalasiIdo+" perc.");
+    }
+    private double bekerData(){
+        String megmunkaltHosszStr=beker("Kérem adja meg a megmunkált hosszt (mm-ben): ");
+        double megmunkaltHossz=Double.parseDouble(megmunkaltHosszStr);
+        String fordulatonkentiElotolasStr=beker("Kérem adja meg a fordulatonkénti előtolást (mm-ben): ");
+        double fordulatonkentiElotolas=Double.parseDouble(fordulatonkentiElotolasStr);
+        String foorsoFordulatszamStr=beker("Kérem adja meg a főorsó fordulatszámát (fordulatszám/perc-ben): ");
+        double foorsoFordulatszam=Double.parseDouble(foorsoFordulatszamStr);
+        return szamol(megmunkaltHossz, fordulatonkentiElotolas, foorsoFordulatszam);
+    }
+    private String beker(String msg){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println(msg);
+        return scanner.nextLine();
+    }
+    private double szamol(double megMunkalthossz, double fordulatonkentiElotolas, double foorsoFordulatszam){
+        double megmunkalasiIdo=megMunkalthossz/(fordulatonkentiElotolas*foorsoFordulatszam);
+        return megmunkalasiIdo;
+    }
+}
